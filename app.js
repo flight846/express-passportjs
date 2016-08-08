7d966c3ea294551c4534cd16edaf61b8c6962dbd
yazid <flight846@gmail.com>
2016-08-08 21:33:39 +0800

    Add navbar headers

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ var app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', {
+    isAuthenticated: false,
+    user: req.user
+  })
+})
+
+app.get('/login', function (req, res) {
+  res.render('login')
 })
 
 var port = process.env.PORT || 3000
